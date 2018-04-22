4ca82773b7a5027a0626c17c5be1cc96cbfc9c83
Roman Klyuev <romankmail@gmail.com>
2018-04-22 09:27:22 +0300

    Leetcode exercises

---
+++ leetcode/min_stack.py
@@ -0,0 +1,72 @@
+# https://leetcode.com/problems/min-stack/description/
+
+"""
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+Example:
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+"""
+
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.obj = list()
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.obj.append(x)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.obj.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.obj[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return min(self.obj)
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+# ++++++++++++++
+
+minStack = MinStack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+print('getMin:', minStack.getMin())  # Returns -3.
+minStack.pop()
+print('top:', minStack.top())  # Returns 0.
+print('getMin:', minStack.getMin())  # Returns -2.
