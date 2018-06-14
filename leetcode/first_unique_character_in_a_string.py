54d6f125a2e3624bf8a9b7dc050cdc5e788bb1fb
Roman Klyuev <romankmail@gmail.com>
2018-06-14 17:19:26 +0300

    Leetcode: top 100 interview, easy

---
+++ leetcode/first_unique_character_in_a_string.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/first-unique-character-in-a-string/description/
+
+"""
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+"""
+
+class Solution:
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        # Time Limit Exceeded
+        """
+        for char in s:
+            if s.count(char) == 1:
+                return s.index(char)
+
+        return -1
+        """
+
+
+sol = Solution()
+print(sol.firstUniqChar('leetcode'))
+print(sol.firstUniqChar('loveleetcode'))
