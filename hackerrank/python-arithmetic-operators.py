5f60b38c706e553a4a5737d107c974cbc900c45a
Roman Klyuev <romankmail@gmail.com>
2018-06-15 18:51:41 +0300

    Hackerrank Python

---
+++ hackerrank/python-arithmetic-operators.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+
+
+def py_math(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    py_math(a, b)
---
+++ hackerrank/python_division.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/python-division/problem
+
+
+def py_div(a, b):
+    print(a // b)
+    print(a / b)
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    py_div(a, b)
