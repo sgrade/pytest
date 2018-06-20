99fb2368879f53957610c002ca82eedfe1f8f2a0
Roman Klyuev <romankmail@gmail.com>
2018-06-20 15:32:22 +0300

    Leetcode

--- leetcode/first_unique_character_in_a_string.py
+++ leetcode/first_unique_character_in_a_string.py
@@ -21,6 +21,31 @@ class Solution:
         :rtype: int
         """
 
+        # https://stackoverflow.com/questions/15495731/best-way-to-find-first-non-repeating-character-in-a-string
+        order = list()
+        counts = dict()
+        for x in s:
+            if x in counts:
+                counts[x] += 1
+            else:
+                counts[x] = 1
+                order.append(x)
+        for x in order:
+            if counts[x] == 1:
+                return s.index(x)
+        return -1
+
+        # One of the solutions based on generators
+        # https://stackoverflow.com/questions/15495731/best-way-to-find-first-non-repeating-character-in-a-string
+        # Time Limit Exceeded
+        """
+        try:
+            non_repeating_char = next(a for a in s if s.count(a) == 1)
+            return s.index(non_repeating_char)
+        except StopIteration:
+            return -1
+        """
+
         # Time Limit Exceeded
         """
         for char in s:
@@ -34,3 +59,4 @@ class Solution:
 sol = Solution()
 print(sol.firstUniqChar('leetcode'))
 print(sol.firstUniqChar('loveleetcode'))
+print(sol.firstUniqChar('llvv'))
