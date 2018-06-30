48b792015ce410a03c90938dd9b05bf0cd07a787
Roman Klyuev <romankmail@gmail.com>
2018-06-30 21:51:31 +0300

    Leetcode

--- leetcode/longest_palindromic_substring.py
+++ leetcode/longest_palindromic_substring.py
@@ -22,6 +22,8 @@ class Solution:
         :rtype: str
         """
 
+        # Time Limit Exceeded
+        """
         def check_palindrome(string):
             if string[::-1] == string:
                 return True
@@ -35,19 +37,24 @@ class Solution:
         else:
 
             s_length = len(s)
-            cur_pal = s[0]  # To store the length of current longest palindrome
             longest_pal = s[0]  # To store the result
             longest_pal_length = 1
-            i = 0
-            j = 1
-            for j in range(1, s_length):
-                if check_palindrome(s[i:j]):
-                    cur_pal = s[i:j]
-                    if len(cur_pal) > longest_pal_length:
-                        longest_pal = cur_pal
-                    j += 1
+            for i in range(0, s_length):
+                for j in range(1, s_length+1):
+                    # print('checking', s[i:j])
+                    if check_palindrome(s[i:j]):
+                        # print('palindrome found:', s[i:j])
+                        cur_pal = s[i:j]
+                        if len(cur_pal) > longest_pal_length:
+                            longest_pal = s[i:j]
+                            longest_pal_length = j-i
+                            # print('longest pal len:', longest_pal_length)
+                            # print('new longest pal:', s[i:j])
+                        j += 1
+                i += 1
 
             return longest_pal
+            """
 
 
 sol = Solution()
@@ -56,4 +63,7 @@ inp = "babad"
 print(sol.longestPalindrome(inp) == "bab")
 
 inp = "cbbd"
+print(sol.longestPalindrome(inp) == "bb")
+
+inp = "bb"
 print(sol.longestPalindrome(inp) == "bb")
