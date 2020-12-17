2f4e901da8a8be3b2bb6d698d15a3da967620e7f
Roman K <sgrade@users.noreply.github.com>
2020-12-17 23:17:36 +0100

    codeforces

--- codeforces/115A.py
+++ codeforces/115A.py
@@ -1,7 +1,17 @@
 # A. Party
 
-st = set()
-for _ in range(int(input())):
-    st.add(int(input()))
+# Editorial - https://codeforces.com/blog/entry/2584
+# Solution based on https://codeforces.com/contest/115/submission/57934041
 
-print(len(st))
+n = int(input())
+p = [int(input()) for i in range(n)]
+
+ans = 0
+for i in range(n):
+    current = 0
+    while i >= 0:
+        i = p[i] - 1
+        current += 1
+    ans = max(ans, current)
+
+print(ans)
