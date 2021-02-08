f0581db230b6b071550cd70ffc6683602ff4ccd9
Roman K <sgrade@users.noreply.github.com>
2021-02-08 20:05:09 +0100

    codeforces

--- codeforces/302A.py
+++ codeforces/302A.py
@@ -4,10 +4,14 @@ n, m = map(int, input().split())
 a = list(map(int, input().split()))
 positives = a.count(1)
 negatives = a.count(-1)
+mn = min(positives, negatives)
 
+output = list()
 for _ in range(m):
     l, r = map(int, input().split())
-    if (r - l + 1) % 2 == 0 and (r - l + 1) / 2 <= min(positives, negatives):
-        print(1)
+    if (r - l + 1) % 2 == 0 and (r - l + 1) / 2 <= mn:
+        output.append('1')
     else:
-        print(0)
+        output.append('0')
+
+print('\n'.join(output))
