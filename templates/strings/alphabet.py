b6fceb82dd9607ebfbc74aa50215adc9737e1ffd
Roman Klyuev <sgrade@users.noreply.github.com>
2021-06-15 17:27:42 +0000

    templates/strings/alphabet

---
+++ templates/strings/alphabet.py
@@ -0,0 +1,7 @@
+# https://stackoverflow.com/questions/16060899/alphabet-range-in-python
+
+from string import ascii_lowercase
+# 'abcdefghijklmnopqrstuvwxyz'
+
+# if you need a list
+list(ascii_lowercase)
