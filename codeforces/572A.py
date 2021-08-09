0b644346b41a54256dc8aabb3ed0e84ee50e5bf3
Roman Klyuev <sgrade@users.noreply.github.com>
2021-08-09 16:45:14 +0000

    codeforces

---
+++ codeforces/572A.py
@@ -0,0 +1,11 @@
+# A. Arrays
+
+na, nb = map(int, input().split())
+k, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+if a[k-1] < b[nb - m]:
+    print("YES")
+else:
+    print("NO")
