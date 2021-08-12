14cd3395b70ac0ea5f27186fdf47cc773a761958
Roman Klyuev <sgrade@users.noreply.github.com>
2021-08-12 18:02:21 +0000

    codeforces

---
+++ codeforces/1530A.py
@@ -0,0 +1,9 @@
+# A. Binary Decimal
+
+for _ in range(int(input())):
+    s = input()
+
+    # Editirial - https://codeforces.com/blog/entry/92951
+    # Idea is from https://codeforces.com/contest/1530/submission/122921927
+    ans = max(s)
+    print(ans)
