9b84c148d35c1759f6a959d505388155cf2e67d4
Roman Klyuev <sgrade@users.noreply.github.com>
2021-08-20 19:05:47 +0000

    misc

--- misc/apache_log_parsing.py
+++ misc/apache_log_parsing.py
@@ -1,21 +1,28 @@
+#!/usr/bin/env python3
+
 """ Apache log parsing exercise
 https://0xbharath.github.io/python-foundations/log_parsing/index.html
 """
 
 import sys
 
-try:
-    with open(sys.argv[2]) as f:
+def main():
+    try:
+        with open(sys.argv[2]) as f:
 
-        lines = f.read().splitlines()
+            lines = f.read().splitlines()
 
-        if sys.argv[1] == '-u':
-            unique_clients = {x.split()[0] for x in lines}
-            print("Unique clients:", *unique_clients, sep="\n")
+            if sys.argv[1] == '-u':
+                unique_clients = {x.split()[0] for x in lines}
+                print("Unique clients:", *unique_clients, sep="\n")
 
-        if sys.argv[1] == '-t':
-            lines.sort(key=lambda x: int(x.split()[2]))
-            print("Sorted by requests: ")
-            print("\n".join(lines))
-except:
-    raise
+            if sys.argv[1] == '-t':
+                lines.sort(key=lambda x: int(x.split()[2]))
+                print("Sorted by requests: ")
+                print("\n".join(lines))
+    except:
+        raise
+
+
+if __name__ == '__main__':
+    main()
