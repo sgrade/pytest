3729fd898423951a3ad941dabf1661d297bc28de
sgrade <sgrade@users.noreply.github.com>
2023-04-09 18:05:08 +0000

    leetcode

--- leetcode/design-compressed-string-iterator.py
+++ leetcode/design-compressed-string-iterator.py
@@ -1,31 +1,33 @@
 # 604. Design Compressed String Iterator
 # https://leetcode.com/problems/design-compressed-string-iterator/
 
+# Optimized with ideas from Leetcode's Sample 30 ms submission
 class StringIterator:
 
     def __init__(self, compressedString: str):
-        self.s = ""
-        i = 0
-        while i < len(compressedString):
-            j = i + 1
-            cnt = 0
-            while j < len(compressedString) and compressedString[j].isdigit():
-                cnt = cnt * 10 + int(compressedString[j])
-                j += 1
-            self.s += compressedString[i] * cnt
-            i = j
+        self.s = compressedString
         self.idx = 0
-
+        self.cnt = 0
+        self.next_idx = 0
+        
     def next(self) -> str:
-        has_next = self.hasNext()
-        if not has_next:
-            return ' '
-        ch = self.s[self.idx]
-        self.idx += 1
-        return ch
+        if self.cnt > 0:
+            self.cnt -= 1
+            return self.s[self.idx]
+        else:
+            if self.next_idx == len(self.s):
+                return ' '
+            self.cnt = 0
+            self.idx = self.next_idx
+            self.next_idx += 1
+            while self.next_idx < len(self.s) and self.s[self.next_idx].isdigit():
+                self.cnt = self.cnt * 10 + int(self.s[self.next_idx])
+                self.next_idx += 1
+            self.cnt -= 1
+            return self.s[self.idx]
         
     def hasNext(self) -> bool:
-        return self.idx < len(self.s)
+        return self.cnt > 0 or self.next_idx < len(self.s)
 
 # Your StringIterator object will be instantiated and called as such:
 # obj = StringIterator(compressedString)
