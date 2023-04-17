fd21030d4a4b9943f426f75336aa9ab3700afb20
sgrade <sgrade@users.noreply.github.com>
2023-04-17 15:24:48 +0000

    leetcode

--- leetcode/design-snake-game.py
+++ leetcode/design-snake-game.py
@@ -4,42 +4,31 @@
 from collections import deque
 
 
+# Optimized with ideas from Leetcode's Sample 209 ms submission
 class SnakeGame:
 
     def __init__(self, width: int, height: int, food: list[list[int]]):
         self.score = 0
         self.cols = width
         self.rows = height
-        self.food = deque(food)
-        self.head_row = 0
-        self.head_col = 0
+        self.food = food
         self.body = deque()
         self.body.append([0, 0])
-        self.directions = {
-            "U": [-1, 0],
-            "D": [1, 0],
-            "L": [0, -1],
-            "R": [0, 1]
-        }
+        self.directions = {"U": (-1, 0), "D": (1, 0), "L": (0, -1), "R": (0, 1)}
 
     def move(self, direction: str) -> int:
-        self.head_row += self.directions[direction][0]
-        self.head_col += self.directions[direction][1]
+        head_row = self.body[0][0] + self.directions[direction][0]
+        head_col = self.body[0][1] + self.directions[direction][1]
+        new_head = [head_row, head_col]
 
-        if self.head_row < 0 or self.head_row == self.rows or self.head_col < 0 or self.head_col == self.cols:
-            return -1
-        
-        new_head = [self.head_row, self.head_col]
-
-        if len(self.food) > 0 and self.head_row == self.food[0][0] and self.head_col == self.food[0][1]:
+        if self.score < len(self.food) and new_head == self.food[self.score]:
             self.score += 1
-            self.food.popleft()
-            self.body.appendleft(new_head)
-            return self.score
-        
-        self.body.pop()
-        if new_head in self.body:
+        else:
+            self.body.pop()
+
+        if new_head in self.body or head_row < 0 or head_row == self.rows or head_col < 0 or head_col == self.cols:
             return -1
+            
         self.body.appendleft(new_head)
         return self.score
 
