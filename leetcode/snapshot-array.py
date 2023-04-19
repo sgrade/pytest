29e452e5a1cc8e479eed7b2d6cf865b8c74d955f
sgrade <sgrade@users.noreply.github.com>
2023-04-19 21:13:01 +0000

    leetcode

--- leetcode/snapshot-array.py
+++ leetcode/snapshot-array.py
@@ -3,26 +3,24 @@
 
 import bisect
 
-# Memory Limit Exceeded
-# Based on Leetcode's Sample 434 ms submission
+
+# My own solution optimized with ideas from Leetcode's Sample 532 ms submission
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.snap_ar = {}
-        self.current = {}
+        self.snap_ar = [[[0, 0]] for _ in range(length)]
         self.snap_id = 0
 
     def set(self, index: int, val: int) -> None:
-        self.current[index] = val
+        self.snap_ar[index].append([self.snap_id, val])
 
     def snap(self) -> int:
-        self.snap_ar[self.snap_id] = dict(self.current)
         self.snap_id += 1
         return self.snap_id - 1
 
     def get(self, index: int, snap_id: int) -> int:
-        return self.snap_ar[snap_id].get(index, 0)
-
+        idx = bisect.bisect(self.snap_ar[index], snap_id, key=lambda p: p[0]) - 1
+        return self.snap_ar[index][idx][1]
 
 # Your SnapshotArray object will be instantiated and called as such:
 # obj = SnapshotArray(length)
