2015e6d55f39fbcc9705fb0feb0c94ba4cf4c6e5
sgrade <sgrade@users.noreply.github.com>
2023-04-19 20:23:20 +0000

    leetcode

---
+++ leetcode/snapshot-array.py
@@ -0,0 +1,36 @@
+# 1146. Snapshot Array
+# https://leetcode.com/problems/snapshot-array/
+
+import bisect
+
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.snap_ar = [[[0, 0]] for i in range(length)]
+        self.snap_id = 0
+
+    def set(self, index: int, val: int) -> None:
+        prev_snap_id = self.snap_ar[index][-1][0]
+        if prev_snap_id == self.snap_id:
+            self.snap_ar[index][-1][1] = val
+        else:
+            self.snap_ar[index].append([self.snap_id, val])
+
+    def snap(self) -> int:
+        self.snap_id += 1
+        return self.snap_id - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        if not self.snap_ar[index]:
+            return 0
+        idx = bisect.bisect(self.snap_ar[index], snap_id, key=lambda p: p[0])
+        if idx != 0:
+            idx -= 1
+        return self.snap_ar[index][idx][1]
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
