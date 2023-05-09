75b1651bdd9fa83243d792eb38f304da6c41f48d
sgrade <sgrade@users.noreply.github.com>
2023-05-09 14:14:41 +0000

    leetcode

--- leetcode/minesweeper_bfs.py
+++ leetcode/minesweeper_bfs.py
@@ -10,9 +10,15 @@ class Solution:
         cols = len(board[0])
 
         diff_r = diff_c = [-1, 0, 1]
-
-        def count_mines(row, col):
+            
+        q = deque()
+        q.append(click)
+        while q:
+            row, col = q.popleft()
+            if board[row][col] != 'E':
+                continue
             adjacent_mines = 0
+            adjacent_cells = []
             for r in diff_r:
                 for c in diff_c:
                     if r == 0 and c == 0:
@@ -23,27 +29,11 @@ class Solution:
                         continue
                     if board[rr][cc] == 'M':
                         adjacent_mines += 1
-            return adjacent_mines
-            
-        q = deque()
-        q.append(click)
-        while len(q) > 0:
-            row, col = q.popleft()
-            if board[row][col] != 'E':
-                continue
-            adjacent_mines = count_mines(row, col)
+                    else:
+                        adjacent_cells.append([rr, cc])
             if adjacent_mines != 0:
                 board[row][col] = str(adjacent_mines)
-                continue
-            board[row][col] = 'B'
-            for r in diff_r:
-                for c in diff_c:
-                    if r == 0 and c == 0:
-                        continue
-                    rr = row + r
-                    cc = col + c
-                    if rr < 0 or rr >= rows or cc < 0 or cc >= cols or board[rr][cc] != 'E':
-                        continue
-                    q.append([rr, cc])
-        
+            else:
+                board[row][col] = 'B'
+                q.extend(adjacent_cells)
         return board
