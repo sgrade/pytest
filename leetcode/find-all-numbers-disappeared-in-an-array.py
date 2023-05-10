4b67000c8cb9458fb9c752900e8a6905cb4d2af4
sgrade <sgrade@users.noreply.github.com>
2023-05-10 21:56:05 +0000

    leetcode

---
+++ leetcode/find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,14 @@
+# 448. Find All Numbers Disappeared in an Array
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+from collections import Counter
+
+
+class Solution:
+    def findDisappearedNumbers(self, nums: list[int]) -> list[int]:
+        cntr = Counter(nums)
+        ans = []
+        for x in range(1, len(nums) + 1):
+            if x not in cntr:
+                ans.append(x)
+        return ans
