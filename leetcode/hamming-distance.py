50370ad7ba2a14fd809c7c13d3c0e12da5185a10
sgrade <sgrade@users.noreply.github.com>
2023-05-13 18:42:27 +0000

    leetcode

--- leetcode/hamming-distance.py
+++ leetcode/hamming-distance.py
@@ -1,12 +1,13 @@
 # 461. Hamming Distance
 # https://leetcode.com/problems/hamming-distance/
 
+# From Editorial's Approach 2: Bit Shift
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
+        xor = x ^ y
         ans = 0
-        while x or y:
-            if x & 1 != y & 1:
+        while xor:
+            if xor & 1:
                 ans += 1
-            x >>= 1
-            y >>= 1
+            xor = xor >> 1
         return ans
