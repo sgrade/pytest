cdb3b0498301dac1df445d5c0303ce6451c9e527
sgrade <sgrade@users.noreply.github.com>
2025-10-24 19:37:51 +0000

    leetcode

--- leetcode/maximum_sum_with_at_most_k_elements.py
+++ leetcode/maximum_sum_with_at_most_k_elements.py
@@ -1,16 +1,14 @@
 # 3462. Maximum Sum With at Most K Elements
 # https://leetcode.com/problems/maximum-sum-with-at-most-k-elements/
 
-import heapq
 from typing import List
 
 
 class Solution:
-    def maxSum(self, grid: List[List[int]], limits: List[int], k: int) -> int:
-        sorted_grid = [sorted(row, reverse=True) for row in grid]
-        
+    def maxSum(self, grid: List[List[int]], limits: List[int], k: int) -> int:       
         largest = []
         for i in range(len(grid)):
-            largest.extend(sorted_grid[i][:limits[i]])
-
-        return sum(heapq.nlargest(k, largest))
+            sorted_row = sorted(grid[i], reverse=True)
+            largest.extend(sorted_row[:limits[i]])
+        largest.sort(reverse=True)
+        return sum(largest[:k])
