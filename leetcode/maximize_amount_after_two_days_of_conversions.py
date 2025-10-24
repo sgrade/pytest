8125decec0144c5343a6a13f0ff0898113908f57
sgrade <sgrade@users.noreply.github.com>
2025-10-24 20:48:32 +0000

    leetcode

--- leetcode/maximize_amount_after_two_days_of_conversions.py
+++ leetcode/maximize_amount_after_two_days_of_conversions.py
@@ -5,47 +5,41 @@ from typing import List
 from collections import defaultdict, deque
 
 
-# Based on https://leetcode.com/problems/maximize-amount-after-two-days-of-conversions/solutions/6147735/easy-bfs
 class Solution:
     def maxAmount(self, initialCurrency: str, pairs1: List[List[str]], rates1: List[float], pairs2: List[List[str]], rates2: List[float]) -> float:
-        adj1 = defaultdict(list)
-        adj2 = defaultdict(list)
-        for i, p in enumerate(pairs1):
-            cur1, cur2 = p
-            rate = rates1[i]
-            adj1[cur1].append((cur2, rate))
-            adj1[cur2].append((cur1, 1 / rate))
-        for i, p in enumerate(pairs2):
-            cur1, cur2 = p
-            rate = rates2[i]
-            adj2[cur1].append((cur2, rate))
-            adj2[cur2].append((cur1, 1 / rate))
-        max_amount_day1 = self.bfs(adj1, initialCurrency, 1.0)
-        ans = 0.0
-        for cur, amt in max_amount_day1.items():
-            max_amount_day2 = self.bfs(adj2, cur, amt)
-            ans = max(ans, max_amount_day2[initialCurrency])
-        return ans
-
+        # Build graphs for both days
+        adj1 = self.build_graph(pairs1, rates1)
+        adj2 = self.build_graph(pairs2, rates2)
         
-    def bfs(self, adj, start_cur, start_amount):
-        max_amount = defaultdict(float)
-        max_amount[start_cur] = start_amount
-        q = deque([(start_cur, start_amount)])
-        while q:
-            cur, amt = q.popleft()
-            for nxt_cur, rate in adj[cur]:
-                nxt_amt = amt * rate
-                if nxt_amt > max_amount[nxt_cur]:
-                    max_amount[nxt_cur] = nxt_amt
-                    q.append((nxt_cur, nxt_amt))
+        # Day 1: Find max amount for each reachable currency
+        day1_amounts = self.bfs(adj1, initialCurrency, 1.0)
+        
+        # Day 2: Try converting back from each currency
+        ans = 1.0  # At minimum, we can keep initial currency
+        for currency, amount in day1_amounts.items():
+            day2_amounts = self.bfs(adj2, currency, amount)
+            if initialCurrency in day2_amounts:
+                ans = max(ans, day2_amounts[initialCurrency])
+        
+        return ans
+    
+    def build_graph(self, pairs, rates):
+        graph = defaultdict(list)
+        for (cur1, cur2), rate in zip(pairs, rates):
+            graph[cur1].append((cur2, rate))
+            graph[cur2].append((cur1, 1 / rate))
+        return graph
+    
+    def bfs(self, graph, start_currency, start_amount):
+        max_amount = {start_currency: start_amount}
+        queue = deque([(start_currency, start_amount)])
+        
+        while queue:
+            current_currency, current_amount = queue.popleft()
+            for next_currency, rate in graph[current_currency]:
+                next_amount = current_amount * rate
+                if next_amount > max_amount.get(next_currency, 0):
+                    max_amount[next_currency] = next_amount 
+                    queue.append((next_currency, next_amount))
+        
         return max_amount
-
-
-if __name__ == "__main__":
-    initialCurrency = "C"
-    pairs1 = [["C","OX"]]
-    rates1 = [1.5]
-    pairs2 = [["C","OX"]]
-    rates2 = [9.4]
-    print(Solution().maxAmount(initialCurrency, pairs1, rates1, pairs2, rates2))
