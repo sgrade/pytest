9f864209bc32c159fabcdd57899b9d84a7e2b4ee
sgrade <sgrade@users.noreply.github.com>
2025-11-24 12:16:56 +0000

    leetcode

--- leetcode/binary_prefix_divisible_by_5.py
+++ leetcode/binary_prefix_divisible_by_5.py
@@ -7,10 +7,10 @@ from typing import List
 class Solution:
     def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
         n = len(nums)
-        ans = [0] * n
+        ans = [False] * n
         sm = 0
         for i in range(n):
             sm *= 2
-            sm += nums[i]
-            ans[i] = sm % 5 == 0
+            sm = (sm + nums[i]) % 5
+            ans[i] = sm == 0
         return ans
