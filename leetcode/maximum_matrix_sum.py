45c6be71e3df84526d86283d686031e1825b79b6
sgrade <sgrade@users.noreply.github.com>
2026-01-05 08:18:39 +0000

    leetcode

--- leetcode/maximum_matrix_sum.py
+++ leetcode/maximum_matrix_sum.py
@@ -6,19 +6,14 @@ class Solution:
     def maxMatrixSum(self, matrix: list[list[int]]) -> int:
         total = 0
         min_abs = 10**9
-        negatives, zeroes = 0, 0
+        negatives = 0
         for row in matrix:
             for num in row:
+                cur_abs = abs(num)
+                total += cur_abs
+                min_abs = min(min_abs, cur_abs)
                 if num < 0:
                     negatives += 1
-                    cur_abs = abs(num)
-                    min_abs = min(min_abs, cur_abs)
-                    total += cur_abs
-                elif num > 0:
-                    total += num
-                    min_abs = min(min_abs, num)
-                else:
-                    zeroes += 1
-        if zeroes == 0 and negatives % 2 != 0:
+        if negatives % 2 != 0:
             total -= 2 * min_abs
         return total
