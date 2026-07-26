64e47c6ab0701c8ff847ff1f1289bee174a50db3
sgrade <sgrade@users.noreply.github.com>
2026-07-26 15:57:14 +0000

    leetcode

--- leetcode/maximum_product_of_three_numbers.py
+++ leetcode/maximum_product_of_three_numbers.py
@@ -4,7 +4,15 @@
 
 class Solution:
     def maximumProduct(self, nums: list[int]) -> int:
-        nums.sort(reverse=True)
-        if nums[0] < 0:
-            return nums[0] * min(nums[1] * nums[2], nums[-1] * nums[-2])
-        return nums[0] * max(nums[1] * nums[2], nums[-1] * nums[-2])
+        mx: list = [-1000, -1000, -1000]
+        mn: list = [1000, 1000]
+        for num in nums:
+            if num > mx[0]:
+                mx.append(num)
+                mx.sort()
+                mx = mx[1:]
+            if num < mn[1]:
+                mn.append(num)
+                mn.sort()
+                mn = mn[:2]
+        return max(mx[-1] * mx[-2] * mx[-3], mx[-1] * mn[0] * mn[1])
