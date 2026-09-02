1303869067f9899a6786fb3d89fb42eedac1f4c1
sgrade <sgrade@users.noreply.github.com>
2026-09-02 10:08:16 +0000

    leetcode

---
+++ leetcode/construct_uniform_parity_array_i.py
@@ -0,0 +1,7 @@
+# 3875. Construct Uniform Parity Array I
+# https://leetcode.com/problems/construct-uniform-parity-array-i/
+
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
