3fb3baf1e2244359a26c5b00785417af4fea8ca1
sgrade <sgrade@users.noreply.github.com>
2026-09-04 15:11:45 +0000

    leetcode

--- leetcode/smallest_stable_index_i.py
+++ leetcode/smallest_stable_index_i.py
@@ -5,19 +5,18 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         n = len(nums)
-        cur_max, cur_min = nums[0], nums[n - 1]
 
-        mx = [0] * n
-        mn = [0] * n
-        for i in range(n):
-            cur_max = max(cur_max, nums[i])
-            mx[i] = cur_max
+        # suffix_min[i] is the minimum of nums[i:].
+        suffix_min = [0] * n
+        cur_min = nums[-1]
         for i in range(n - 1, -1, -1):
             cur_min = min(cur_min, nums[i])
-            mn[i] = cur_min
+            suffix_min[i] = cur_min
 
-        for i in range(n):
-            instability_score = mx[i] - mn[i]
-            if instability_score <= k:
+        # Sweep left to right, tracking the maximum of nums[:i + 1].
+        cur_max = nums[0]
+        for i, num in enumerate(nums):
+            cur_max = max(cur_max, num)
+            if cur_max - suffix_min[i] <= k:
                 return i
         return -1
